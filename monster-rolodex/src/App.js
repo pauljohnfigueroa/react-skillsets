1160c9ba0dd49f53f597100279043f3bfe931051
Paul Figueroa <pjsfigueroa@gmail.com>
2022-11-21 17:16:04 +0800

    Initial commit.

---
+++ monster-rolodex/src/App.js
@@ -0,0 +1,41 @@
+import { Component } from 'react'; // <- this is required by a Class component
+import logo from './logo.svg';
+import './App.css';
+
+// React uses funtions to return HTML
+class App extends Component { // <- this is a Class component
+
+  constructor() {
+    super(); // <- call the constructor of the parent
+
+    this.state = {
+      // monster1: { name: 'Linda' },
+      // monster2: { name: 'Frank' },
+      // monster3: { name: 'Jonie' }
+
+      // converted to array
+
+      monsters: [
+        { id: '122sdf', name: 'Linda' },
+        { id: '122sddf', name: 'Frank' },
+        { id: '122sfff', name: 'Jonie' },
+        { id: '122sdfga', name: 'Juice' },
+      ]
+    };
+
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        {
+          this.state.monsters.map((monster) => { // map() - do this operation on each item in the array
+            return <div key={monster.id}><h2>{monster.name}</h2></div>
+          })
+        }
+      </div >
+    )
+  };
+}
+
+export default App;
