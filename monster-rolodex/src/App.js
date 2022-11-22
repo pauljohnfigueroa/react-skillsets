93c25f5f28061fbd57f127e49935bdebfc82a729
Paul Figueroa <pjsfigueroa@gmail.com>
2022-11-22 18:58:21 +0800

    Complete project.

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -66,6 +66,7 @@ class App extends Component { // <- this is a Class component
 
     return (
       <div className="App" >
+        <h1 className='app-title'>Monsters Rolodex</h1>
         <SearchBox
           onChangeHandler={onSearchChange}
           className="monster-search-box"
--- monster-rolodex/src/components/card-list/card-list.component.jsx
+++ monster-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,16 +1,20 @@
 import { Component } from "react"; // we need this for our class component
+import Card from '../card/card.component';
+
+import './card-list.styles.css';
 
 class CardList extends Component {
 
     render() {
-        console.log('Render from CardList component.');
+        //console.log('Render from CardList component.');
         const { monsters } = this.props;
-
         return (
-            <div> {/* make sure that there is only one top level component */}
-                { // do not forget the bracket when outputing
+            <div className="card-list">
+                { // do not forget the bracket when outputing javascript
                     monsters.map((monster) => {
-                        return <h2 key={monster.id}>{monster.name}</h2>
+                        return (
+                            <Card monster={monster} className="card-container" />
+                        )
                     })
                 }
             </div>
