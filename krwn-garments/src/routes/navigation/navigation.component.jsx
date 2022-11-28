9c77da61a678c411bb3040f36a27ecab56ce4350
Paul Figueroa <pjsfigueroa@gmail.com>
2022-11-28 19:47:57 +0800

    Install firebase.

---
+++ krwn-garments/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,30 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+import { ReactComponent as KrwnLogo } from '../../assets/krwnlogo.svg';
+
+import './navigation.styles.scss';
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className='navigation'>
+                <Link className='logo-container' to="/">
+                    <KrwnLogo className='logo' />
+                </Link>
+
+                <div className='nav-links-container'>
+                    <Link className='nav-link' to="/shop">
+                        SHOP
+                    </Link>
+                    <Link className='nav-link' to="/sign-in">
+                        SIGN IN
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    );
+}
+
+export default Navigation;
