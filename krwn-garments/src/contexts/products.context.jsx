db9a88d0ac59b24ec1c798a9e3582a554b6c4319
Paul Figueroa <pjsfigueroa@gmail.com>
2022-12-04 18:29:47 +0800

    Upload categories data to firebase.

--- krwn-garments/src/contexts/products.context.jsx
+++ krwn-garments/src/contexts/products.context.jsx
@@ -9,21 +9,28 @@
 //     return (<ProductProvider.Provider value={ }>{children}</ProductProvider.Provider>)
 // }
 
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils';
 
-import PRODUCTS from '../shop-data.json';
+// import PRODUCTS from '../shop-data.json';
+import SHOP_DATA from "../shop-data.js";
 
 // initialize
 export const ProductsContext = createContext({
     products: [],
 });
 
-
 export const ProductsProvider = ({ children }) => {
-    const [products, setProducts] = useState(PRODUCTS);
+
+    // We used this to upload data to firestore
+    // useEffect(() => {
+    //     addCollectionAndDocuments('categories', SHOP_DATA);
+    // }, []);
+
+    const [products, setProducts] = useState([]);
     const value = { products };
-    //console.log(value);
-    // {children here pertains to all the 
+    // console.log(value);
+    // {children} pertains to all the 
     // components inside the ProductsProvider (see index.js)}
     return (<ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>)
 }
