990800142b4a43f99eac2c0a135e0e8323583cd5
Paul Figueroa <pjsfigueroa@gmail.com>
2022-12-04 08:30:22 +0800

    Add Add to Cart quantity.

--- krwn-garments/src/contexts/cart.context.jsx
+++ krwn-garments/src/contexts/cart.context.jsx
@@ -1,12 +1,41 @@
-import { createContext, useState } from "react"
+import { createContext, useState } from "react";
+
+
+const addCartItem = (cartItems, productToAdd) => {
+    const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id);
+    console.log(existingCartItem);
+
+    if (existingCartItem) {
+
+        return cartItems.map((cartItem) =>
+            cartItem.id === productToAdd.id
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
+        );
+    }
+
+    return [...cartItems, { ...productToAdd, quantity: 1 }];
+}
+
+
 
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => { }
-})
+    setIsCartOpen: () => { },
+    cartItems: [],
+    addItemToCart: () => { },
+});
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = { isCartOpen, setIsCartOpen };
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd));
+    }
+
+    // expose these values to the children components
+    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
+
     return <CartContext.Provider value={value}> {children} </CartContext.Provider>
 }
