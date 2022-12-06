222106d813d0940c3316b4d7d756e3380edcb559
Paul Figueroa <pjsfigueroa@gmail.com>
2022-12-07 00:17:23 +0800

    Styled cart dropdown component.

--- krwn-garments/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ krwn-garments/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,12 @@
-import Button from '../button/button.component';
+import Button, { BUTTON_TYPE_CLASS } from '../button/button.component';
 
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../../contexts/cart.context';
 import CartItem from '../cart-item/cart-item.component';
-import './cart-dropdown.styles.scss';
+
+import { CartDropdownContainer, EmptyMessage, CartItems } from './cart-dropdown.styles';
 
 const CartDropDown = () => {
 
@@ -17,12 +18,16 @@ const CartDropDown = () => {
     }
 
     return (
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'>
-                {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
-            </div>
-            <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-        </div>
+        <CartDropdownContainer>
+            <CartItems>
+                {
+                    cartItems.length ?
+                        cartItems.map(item => <CartItem key={item.id} cartItem={item} />)
+                        : <EmptyMessage>Your cart is empty.</EmptyMessage>
+                }
+            </CartItems>
+            <Button buttonType={BUTTON_TYPE_CLASS.base} onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
+        </CartDropdownContainer>
     );
 }
 
--- krwn-garments/src/components/product-card/product-card.component.jsx
+++ krwn-garments/src/components/product-card/product-card.component.jsx
@@ -3,7 +3,7 @@ import './product-card.styles.scss';
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
 
-import Button from '../button/button.component';
+import Button, { BUTTON_TYPE_CLASS } from '../button/button.component';
 
 const ProductCard = ({ product }) => {
 
@@ -19,7 +19,7 @@ const ProductCard = ({ product }) => {
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-            <Button buttonType="inverted" onClick={addProductToCart}>Add to Cart</Button>
+            <Button buttonType={BUTTON_TYPE_CLASS.inverted} onClick={addProductToCart}>Add to Cart</Button>
         </div>
     );
 }
