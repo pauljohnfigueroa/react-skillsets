94dafdc4b36e7a9816a762bd43baf48268d1dd0f
Paul Figueroa <pjsfigueroa@gmail.com>
2023-01-21 09:10:30 +0800

    Install Yup for validation.

--- audiophile/src/components/checkout/checkout.component.jsx
+++ audiophile/src/components/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import { Fragment } from 'react'
 import { useSelector } from 'react-redux'
 //import { CartContext } from '../../contexts/cart.context';
 import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 import { selectCartItems, selectCartTotalAmount } from '../../store/cart/cart.selector'
 import FormInput from '../form-input/form-input.component'
@@ -29,195 +30,213 @@ render method cleaner.
     )
 */
 
-const validate = values => {
-  const errors = {}
+// const validate = values => {
+//   const errors = {}
 
-  if (!values.name) {
-    errors.name = 'Required'
-  } else if (values.name.length > 15) {
-    errors.name = 'Must be 15 characters or less'
-  }
+//   if (!values.name) {
+//     errors.name = 'Required'
+//   } else if (values.name.length > 15) {
+//     errors.name = 'Must be 15 characters or less'
+//   }
 
-  if (!values.email) {
-    errors.email = 'Requird'
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Please give a valid email address.'
-  }
+//   if (!values.email) {
+//     errors.email = 'Requird'
+//   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+//     errors.email = 'Please give a valid email address.'
+//   }
 
-  return errors
-}
+//   if (!values.phone) {
+//     errors.phone = 'Requird'
+//   } else if (!/^[0-9]/.test(values.phone)) {
+//     errors.phone = 'Numbers only.'
+//   }
+
+//   return errors
+// }
 
 const CheckOut = () => {
-  // const { cartItems, cartTotalAmount } = useContext(CartContext);
-  const cartItems = useSelector(selectCartItems)
-  const cartTotalAmount = useSelector(selectCartTotalAmount)
+    // const { cartItems, cartTotalAmount } = useContext(CartContext);
+    const cartItems = useSelector(selectCartItems)
+    const cartTotalAmount = useSelector(selectCartTotalAmount)
 
-  let cartItemsArray = cartItems.length ? (
-    cartItems.map(cartItem => <CartItem cartItem={cartItem} />)
-  ) : (
-    <h2 class="text-align-center">Your Cart is Empty.</h2>
-  )
+    let cartItemsArray = cartItems.length ? (
+        cartItems.map(cartItem => <CartItem cartItem={cartItem} />)
+    ) : (
+        <h2 class="text-align-center">Your Cart is Empty.</h2>
+    )
 
-  // if (cartItems.length) {
-  //     cartItemsArray = cartItems.map(cartItem => <CartItem cartItem={cartItem} />);
-  // } else {
-  //     cartItemsArray = <h2 class="text-align-center">Your Cart is Empty.</h2>
-  // }
+    // if (cartItems.length) {
+    //     cartItemsArray = cartItems.map(cartItem => <CartItem cartItem={cartItem} />);
+    // } else {
+    //     cartItemsArray = <h2 class="text-align-center">Your Cart is Empty.</h2>
+    // }
 
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      phone: '',
-      address: '',
-      zipcode: '',
-      city: '',
-      country: '',
-      eMoneyNum: '',
-      eMoneyPin: ''
-    },
-    validate,
-    onSubmit: values => {
-      console.log(JSON.stringify(values, null, 2))
-    }
-  })
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            phone: '',
+            address: '',
+            zipcode: '',
+            city: '',
+            country: '',
+            eMoneyNum: '',
+            eMoneyPin: ''
+        },
+        validationSchema: Yup.object({
+            name: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
+            email: Yup.string().email('Please give a valid email address.').required('Required'),
+            phone: Yup.string().matches(/^[0-9]+$/, 'Must be numbers only.').required('Required')
+        }),
+        onSubmit: values => {
+            console.log(JSON.stringify(values, null, 2))
+        }
+    })
 
-  return (
-    <Fragment>
-      <section className="checkout">
-        <section className="checkout-form">
-          <h2>Checkout</h2>
-          <form onSubmit={formik.handleSubmit}>
-            <fieldset className="billing-details">
-              <legend>Billing Details</legend>
-              <div className="text-input">
-                <FormInput
-                  label="Name"
-                  type="text"
-                  name="name"
-                  id="name"
-                  onChange={formik.handleChange}
-                  value={formik.values.name}
-                />
-              </div>
-              {formik.errors.name ? <div>{formik.errors.name}</div> : null}
+    return (
+        <Fragment>
+            <section className="checkout">
+                <section className="checkout-form">
+                    <h2>Checkout</h2>
+                    <form onSubmit={formik.handleSubmit}>
+                        <fieldset className="billing-details">
+                            <legend>Billing Details</legend>
+                            <div className="text-input">
+                                <FormInput
+                                    label="Name"
+                                    type="text"
+                                    name="name"
+                                    id="name"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.name}
+                                />
+                                {formik.touched.name && formik.errors.name ? <div>{formik.errors.name}</div> : null}
+                            </div>
 
-              <div className="text-input">
-                <FormInput
-                  label="Email Address"
-                  type="email"
-                  name="email"
-                  id="email"
-                  onChange={formik.handleChange}
-                  value={formik.values.email}
-                />
-              </div>
-              {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+                            <div className="text-input">
+                                <FormInput
+                                    label="Email Address"
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.email}
+                                />
+                                {formik.touched.email && formik.errors.email ? (
+                                    <div>{formik.errors.email}</div>
+                                ) : null}
+                            </div>
 
-              <div className="text-input">
-                <FormInput
-                  label="Phone Number"
-                  type="text"
-                  name="phone"
-                  id="phone"
-                  onChange={formik.handleChange}
-                  value={formik.values.phone}
-                />
-              </div>
-            </fieldset>
+                            <div className="text-input">
+                                <FormInput
+                                    label="Phone Number"
+                                    type="text"
+                                    name="phone"
+                                    id="phone"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.phone}
+                                />
+                                {formik.touched.phone && formik.errors.phone ? (
+                                    <div>{formik.errors.phone}</div>
+                                ) : null}
+                            </div>
+                        </fieldset>
 
-            <fieldset className="shipping-info">
-              <legend>Shipping info</legend>
-              <div className="text-input address">
-                <FormInput label="Address" type="text" name="address" id="address" />
-              </div>
-              <div className="address-details">
-                <div className="text-input">
-                  <FormInput label="Zip Code" type="text" name="zipcode" id="zipcode" />
-                </div>
-                <div className="text-input">
-                  <FormInput label="City" type="text" name="city" id="city" />
-                </div>
-                <div className="text-input">
-                  <FormInput label="Country" type="text" name="country" id="country" />
-                </div>
-              </div>
-            </fieldset>
+                        <fieldset className="shipping-info">
+                            <legend>Shipping info</legend>
+                            <div className="text-input address">
+                                <FormInput label="Address" type="text" name="address" id="address" />
+                            </div>
+                            <div className="address-details">
+                                <div className="text-input">
+                                    <FormInput label="Zip Code" type="text" name="zipcode" id="zipcode" />
+                                </div>
+                                <div className="text-input">
+                                    <FormInput label="City" type="text" name="city" id="city" />
+                                </div>
+                                <div className="text-input">
+                                    <FormInput label="Country" type="text" name="country" id="country" />
+                                </div>
+                            </div>
+                        </fieldset>
 
-            <fieldset className="payment-details">
-              <legend>Payment Details</legend>
-              <div className="text-input">
-                <label htmlFor="">Payment Method</label>
-              </div>
-              <div className="radio-group">
-                <div className="radio-item">
-                  <FormInput
-                    label="e-Money"
-                    type="radio"
-                    id="e-money"
-                    name="payment-method"
-                    value="e-money"
-                    onChange={() => {}}
-                    checked
-                  />
-                </div>
-                <div className="radio-item">
-                  <FormInput
-                    label="Cash on Delivery"
-                    type="radio"
-                    id="cod"
-                    name="payment-method"
-                    onChange={() => {}}
-                    value="cod"
-                  />
-                </div>
-              </div>
-              <div className="text-input">
-                <FormInput label="e-Money Number" type="text" id="eMoneyNum" name="eMoneyNum" />
-              </div>
-              <div className="text-input">
-                <FormInput label="e-Money PIN" type="text" id="eMoneyPin" name="eMoneyPin" />
-              </div>
-            </fieldset>
+                        <fieldset className="payment-details">
+                            <legend>Payment Details</legend>
+                            <div className="text-input">
+                                <label htmlFor="">Payment Method</label>
+                            </div>
+                            <div className="radio-group">
+                                <div className="radio-item">
+                                    <FormInput
+                                        label="e-Money"
+                                        type="radio"
+                                        id="e-money"
+                                        name="payment-method"
+                                        value="e-money"
+                                        onChange={() => { }}
+                                        checked
+                                    />
+                                </div>
+                                <div className="radio-item">
+                                    <FormInput
+                                        label="Cash on Delivery"
+                                        type="radio"
+                                        id="cod"
+                                        name="payment-method"
+                                        onChange={() => { }}
+                                        value="cod"
+                                    />
+                                </div>
+                            </div>
+                            <div className="text-input">
+                                <FormInput label="e-Money Number" type="text" id="eMoneyNum" name="eMoneyNum" />
+                            </div>
+                            <div className="text-input">
+                                <FormInput label="e-Money PIN" type="text" id="eMoneyPin" name="eMoneyPin" />
+                            </div>
+                        </fieldset>
 
-            <div className="button-container">
-              <Button type="submit" label="Continue & Pay" />
-            </div>
-          </form>
-        </section>
+                        <div className="button-container">
+                            <Button type="submit" label="Continue & Pay" />
+                        </div>
+                    </form>
+                </section>
 
-        <section className="checkout-summary">
-          <h2 className="heading">Summary</h2>
+                <section className="checkout-summary">
+                    <h2 className="heading">Summary</h2>
 
-          {cartItemsArray}
+                    {cartItemsArray}
 
-          <div className="price-details mt-2rem">
-            <div className="price-row">
-              <p className="label">Total</p>
-              <p className="price">$ {cartTotalAmount.toFixed(2).toLocaleString()}</p>
-            </div>
-            <div className="price-row">
-              <p className="label">Shipping</p>
-              <p className="price">$ {(cartTotalAmount * 0.05).toFixed(2).toLocaleString()}</p>
-            </div>
-            <div className="price-row">
-              <p className="label">VAT</p>
-              <p className="price">$ {(cartTotalAmount * 0.12).toFixed(2).toLocaleString()}</p>
-            </div>
-            <div className="price-row">
-              <p className="label">Grand Total</p>
-              <p className="price">
-                ${' '}
-                {(cartTotalAmount * 1.12 + cartTotalAmount * 0.05).toFixed(2).toLocaleString('en')}
-              </p>
-            </div>
-          </div>
-          <div className="button-container">
-            <Button type="button" label="Continue & Pay" />
-          </div>
-        </section>
-      </section>
-    </Fragment>
-  )
+                    <div className="price-details mt-2rem">
+                        <div className="price-row">
+                            <p className="label">Total</p>
+                            <p className="price">$ {cartTotalAmount.toFixed(2).toLocaleString()}</p>
+                        </div>
+                        <div className="price-row">
+                            <p className="label">Shipping</p>
+                            <p className="price">$ {(cartTotalAmount * 0.05).toFixed(2).toLocaleString()}</p>
+                        </div>
+                        <div className="price-row">
+                            <p className="label">VAT</p>
+                            <p className="price">$ {(cartTotalAmount * 0.12).toFixed(2).toLocaleString()}</p>
+                        </div>
+                        <div className="price-row">
+                            <p className="label">Grand Total</p>
+                            <p className="price">
+                                ${' '}
+                                {(cartTotalAmount * 1.12 + cartTotalAmount * 0.05).toFixed(2).toLocaleString('en')}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="button-container">
+                        <Button type="button" label="Continue & Pay" />
+                    </div>
+                </section>
+            </section>
+        </Fragment>
+    )
 }
 export default CheckOut
