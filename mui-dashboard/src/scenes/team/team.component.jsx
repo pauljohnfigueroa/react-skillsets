c97cceb22540240079d90f5c719d5af969543ada
Paul Figueroa <pjsfigueroa@gmail.com>
2023-02-01 23:59:36 +0800

    Finish Datagrid columns configuration

--- mui-dashboard/src/scenes/team/team.component.jsx
+++ mui-dashboard/src/scenes/team/team.component.jsx
@@ -8,19 +8,69 @@ import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined"
 import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined"
 
 import Header from "../../components/header/header.component"
-
+import { color } from "@mui/system"
 
 const Team = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode)
 
     const columns = [
-        { field: 'id', headerName: 'ID' },
-        { field: 'name', headerName: 'Name', flex: 1, cellClassName: 'name-column--cell' },
-        { field: 'email', headerName: 'Email' },
-        { field: 'age', headerName: 'Age', type: "number", headerAlign: 'left', align: 'left' },
-        { field: 'phone', headerName: 'Phone' },
-        { field: 'access', headerName: 'Access' }
+        {
+            field: 'id',
+            headerName: 'ID'
+        },
+        {
+            field: 'name',
+            headerName: 'Name',
+            flex: 1,
+            cellClassName: 'name-column--cell'
+        },
+        {
+            field: 'email',
+            headerName: 'Email',
+            flex: 1
+        },
+        {
+            field: 'age',
+            headerName: 'Age',
+            type: "number",
+            headerAlign: 'left',
+            align: 'left'
+        },
+        {
+            field: 'phone',
+            headerName: 'Phone Number',
+            flex: 1
+        },
+        {
+            field: 'access',
+            headerName: 'Access Level',
+            flex: 1,
+            renderCell: ({ row: { access } }) => {
+                return (
+                    <Box
+                        width="60%"
+                        m="0 auto"
+                        p="5px"
+                        display="flex"
+                        justifyContent="center"
+                        backgroundColor={
+                            access === 'admin'
+                                ? colors.greenAccent[600]
+                                : colors.greenAccent[700]
+                        }
+                        borderRadius="4px"
+                    >
+                        {access === 'admin' && <AdminPanelSettingsOutlinedIcon />}
+                        {access === 'manager' && <SecurityOutlinedIcon />}
+                        {access === 'user' && <LockOpenOutlinedIcon />}
+                        <Typography color={colors.grey[100]} sx={{ ml: "5px" }} >
+                            {access}
+                        </Typography>
+                    </Box >
+                )
+            }
+        }
     ]
 
     return (
@@ -28,7 +78,7 @@ const Team = () => {
             <Header title="Team" subtitle='There is no "I" in Team' />
             <Box
                 m="40px 0 0 0"
-                height="75vh"
+                height="100vh"
             >
                 <DataGrid
                     rows={mockDataTeam}
