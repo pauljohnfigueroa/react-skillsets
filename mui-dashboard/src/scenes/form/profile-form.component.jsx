69e39d085de6c2ca0565f3c56358f2280f7851b5
Paul Figueroa <pjsfigueroa@gmail.com>
2023-02-03 10:48:26 +0800

    Add form initial values and validation schema.

--- mui-dashboard/src/scenes/form/profile-form.component.jsx
+++ mui-dashboard/src/scenes/form/profile-form.component.jsx
@@ -1,10 +1,51 @@
-import { Box, Button, TextField } from '@mui/material'
+import { Box, Button, TextField, useMediaQuery } from '@mui/material'
 import { Formik } from 'formik'
 import * as yup from 'yup'
-import useMediaQuery from '@mui/material'
 import Header from '../../components/header/header.component'
 
+
+const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    contact: "",
+    address1: "",
+    address2: ""
+}
+
+const phoneRegExp = '/^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/'
+
+const userSchema = yup.object().shape({
+    firstName: yup.string().required("Required"),
+    lastName: yup.string().required("Required"),
+    email: yup.string().email("Invalid Email").required("Required"),
+    contact: yup.string().matches(phoneRegExp, "Phone number is not valid.").required("Required"),
+    address1: yup.string().required("Required"),
+    address2: yup.string().required("Required")
+})
+
 const ProfileForm = () => {
-    return (<div>Profile Form</div>)
+
+    const isNonMobile = useMediaQuery("(min-width: 600px)")
+
+    const handleFormSubmit = (values) => {
+        console.log(values)
+    }
+
+    return <Box m="20px">
+        <Formik
+            onSubmit={handleFormSubmit}
+            initialValues={initialValues}
+            validationSchema={userSchema}
+        >
+            {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
+                <form onSubmit={handleSubmit}>
+                    <Box>
+
+                    </Box>
+                </form>
+            )}
+        </Formik>
+    </Box>
 }
 export default ProfileForm
