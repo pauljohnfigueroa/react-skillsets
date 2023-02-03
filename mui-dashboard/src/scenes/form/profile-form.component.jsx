4e70198941cbcf8ba013aecb4df3b493dde777cc
Paul Figueroa <pjsfigueroa@gmail.com>
2023-02-03 12:02:41 +0800

    Complete create user profile form.

--- mui-dashboard/src/scenes/form/profile-form.component.jsx
+++ mui-dashboard/src/scenes/form/profile-form.component.jsx
@@ -13,13 +13,14 @@ const initialValues = {
     address2: ""
 }
 
-const phoneRegExp = '/^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/'
+// const phoneRegExp = '/^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/'
 
 const userSchema = yup.object().shape({
     firstName: yup.string().required("Required"),
     lastName: yup.string().required("Required"),
     email: yup.string().email("Invalid Email").required("Required"),
-    contact: yup.string().matches(phoneRegExp, "Phone number is not valid.").required("Required"),
+    // contact: yup.string().matches(phoneRegExp, "Phone number is not valid.").required("Required"),
+    contact: yup.string().required("Required"),
     address1: yup.string().required("Required"),
     address2: yup.string().required("Required")
 })
@@ -62,7 +63,74 @@ const ProfileForm = () => {
                             helperText={touched.firstName && errors.firstName}
                             sx={{ gridColumn: "span 2" }}
                         />
-
+                        <TextField
+                            fullWidth
+                            variant="filled"
+                            type="text"
+                            label="Last Name"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.lastName}
+                            name="lastName"
+                            error={!!touched.lastName && !!errors.lastName}
+                            helperText={touched.lastName && errors.lastName}
+                            sx={{ gridColumn: "span 2" }}
+                        />
+                        <TextField
+                            fullWidth
+                            variant="filled"
+                            type="email"
+                            label="Email Address"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.email}
+                            name="email"
+                            error={!!touched.email && !!errors.email}
+                            helperText={touched.email && errors.email}
+                            sx={{ gridColumn: "span 4" }}
+                        />
+                        <TextField
+                            fullWidth
+                            variant="filled"
+                            type="text"
+                            label="Contact Number"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.contact}
+                            name="contact"
+                            error={!!touched.contact && !!errors.contact}
+                            helperText={touched.contact && errors.contact}
+                            sx={{ gridColumn: "span 4" }}
+                        />
+                        <TextField
+                            fullWidth
+                            variant="filled"
+                            type="text"
+                            label="Address 1"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.address1}
+                            name="address1"
+                            error={!!touched.address1 && !!errors.address1}
+                            helperText={touched.address1 && errors.address1}
+                            sx={{ gridColumn: "span 4" }}
+                        />
+                        <TextField
+                            fullWidth
+                            variant="filled"
+                            type="text"
+                            label="Address 2"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.address2}
+                            name="address2"
+                            error={!!touched.address2 && !!errors.address2}
+                            helperText={touched.address2 && errors.address2}
+                            sx={{ gridColumn: "span 4" }}
+                        />
+                    </Box>
+                    <Box display="flex" justifyContent="end" mt="20px">
+                        <Button type="submit" color="secondary" variant="contained">Create New User</Button>
                     </Box>
                 </form>
             )}
