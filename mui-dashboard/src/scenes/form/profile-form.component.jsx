3534f0a07f84224681f8b76689d3845a7d04fae8
Paul Figueroa <pjsfigueroa@gmail.com>
2023-02-03 11:25:11 +0800

    Prep profile form textfields.

--- mui-dashboard/src/scenes/form/profile-form.component.jsx
+++ mui-dashboard/src/scenes/form/profile-form.component.jsx
@@ -33,6 +33,7 @@ const ProfileForm = () => {
     }
 
     return <Box m="20px">
+        <Header title="Create User" subtitle="Create a New User Profile" />
         <Formik
             onSubmit={handleFormSubmit}
             initialValues={initialValues}
@@ -40,7 +41,27 @@ const ProfileForm = () => {
         >
             {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
                 <form onSubmit={handleSubmit}>
-                    <Box>
+                    <Box
+                        display="grid"
+                        gap="30px"
+                        gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                        sx={{
+                            "& > div": { gridColumn: isNonMobile ? undefined : "span 4" }
+                        }}
+                    >
+                        <TextField
+                            fullWidth
+                            variant="filled"
+                            type="text"
+                            label="First Name"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.firstName}
+                            name="firstName"
+                            error={!!touched.firstName && !!errors.firstName}
+                            helperText={touched.firstName && errors.firstName}
+                            sx={{ gridColumn: "span 2" }}
+                        />
 
                     </Box>
                 </form>
