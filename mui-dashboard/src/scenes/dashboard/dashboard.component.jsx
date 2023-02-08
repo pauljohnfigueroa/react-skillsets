c235710dec976060122114b6172c01d82cb53911
Paul Figueroa <pjsfigueroa@gmail.com>
2023-02-08 19:54:06 +0800

    Complete the revenue generate and recent transactions card.

--- mui-dashboard/src/scenes/dashboard/dashboard.component.jsx
+++ mui-dashboard/src/scenes/dashboard/dashboard.component.jsx
@@ -150,11 +150,67 @@ const Dashboard = () => {
               <Typography variant="h5" fontWeight="600" color={colors.grey[100]}>
                 Revenue Generated
               </Typography>
-              <Typography variant="h5" fontWeight="600" color={colors.greenAccent[100]}>
+              <Typography variant="h5" fontWeight="bold" color={colors.greenAccent[100]}>
                 $902,044.00
               </Typography>
             </Box>
+            <Box>
+              <IconButton>
+                <DownloadOutlinedIcon
+                  sx={{
+                    fontSize: '26px',
+                    color: colors.greenAccent[400]
+                  }}
+                />
+              </IconButton>
+            </Box>
           </Box>
+
+          <Box height="250px" mt="-20px">
+            <LineChart isDashboard={true} />
+          </Box>
+        </Box>
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+          overflow="auto"
+        >
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            borderBottom={`4px solid ${colors.greenAccent[500]}`}
+            color={colors.grey[100]}
+            p="15px"
+          >
+            <Typography color={colors.grey[100]} variant="h5" fontWeight="600">
+              Recent Transactions
+            </Typography>
+          </Box>
+          {mockTransactions.map((transaction, index) => (
+            <Box
+              key={`${transaction.txId} - ${index}`}
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              borderBottom={`4px solid ${colors.greenAccent[500]}`}
+              p="15px"
+            >
+              <Box>
+                <Typography color={colors.greenAccent[500]} variant="h5" fontWeight="bold">
+                  {transaction.txId}
+                </Typography>
+              </Box>
+              <Box>
+                <Typography color={colors.grey[100]}>{transaction.user}</Typography>
+              </Box>
+              <Box color={colors.grey[100]}>{transaction.date}</Box>
+              <Box backgroundColor={colors.greenAccent[500]} p="5px 10px" borderRadius="4px">
+                {transaction.cost}
+              </Box>
+            </Box>
+          ))}
         </Box>
       </Box>
     </Box>
