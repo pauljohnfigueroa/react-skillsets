d73d3bdd11df80d582e00e7ef15f6c7c9abfe6f7
Paul Figueroa <pjsfigueroa@gmail.com>
2023-03-20 08:36:03 +0800

    Convert the dialog template to Formik.

--- mui-dashboard/src/components/dialog/ConfirmDialog.component.jsx
+++ mui-dashboard/src/components/dialog/ConfirmDialog.component.jsx
@@ -6,7 +6,14 @@ import DialogContent from '@mui/material/DialogContent'
 import DialogContentText from '@mui/material/DialogContentText'
 import DialogTitle from '@mui/material/DialogTitle'
 
+import { Formik, Form } from 'formik'
+// import * as yup from 'yup'
+
 const ConfirmDialog = ({ open, handleClose, handleConfirm }) => {
+  const handleConfirmSubmit = values => {
+    console.log('Confirmed delete', values._id)
+  }
+
   return (
     <div>
       <Dialog open={open}>
@@ -16,20 +23,28 @@ const ConfirmDialog = ({ open, handleClose, handleConfirm }) => {
             You are going to delete an item from the database. Please type <em>delete</em> to
             confirm.
           </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="confirm"
-            label="Confirmation"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
+          <Formik onSubmit={(values, actions) => handleConfirmSubmit(values)} initialValues="">
+            {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
+              <Form>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="confirm"
+                  label="Confirmation"
+                  type="text"
+                  fullWidth
+                  variant="standard"
+                />
+                <DialogActions>
+                  <Button onClick={handleClose}>Cancel</Button>
+                  <Button type="submit" sx={{ minWidth: 100 }} variant="contained">
+                    Delete
+                  </Button>
+                </DialogActions>
+              </Form>
+            )}
+          </Formik>
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleConfirm}>Delete</Button>
-        </DialogActions>
       </Dialog>
     </div>
   )
