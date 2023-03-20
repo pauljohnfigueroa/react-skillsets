7bf0b21c6239990ca129071cf28cc14390a505ef
Paul Figueroa <pjsfigueroa@gmail.com>
2023-03-20 10:58:02 +0800

    Add datagrid re-renderafter creating users.

--- mui-dashboard/src/hooks/useRegisterUser.js
+++ mui-dashboard/src/hooks/useRegisterUser.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { useAuthContext } from './useAuthContext'
+import { useUsersContext } from "./useUsersContext";
 
 export const useRegisterUser = () => {
     const [error, setError] = useState(null)
     const [isLoading, setIsLoading] = useState(false)
     const { user } = useAuthContext()
+    const { dispatch } = useUsersContext()
 
     const registerUser = async (email, name, password, phone, roles) => {
         setIsLoading(true)
@@ -30,6 +32,20 @@ export const useRegisterUser = () => {
         }
 
         if (response.ok) {
+
+            const fetchUsers = async () => {
+                const response = await fetch(`http://localhost:4000/api/user`, {
+                    headers: {
+                        Authorization: `Bearer ${user.token}`
+                    }
+                })
+                const users = await response.json()
+                // Re-render datagrid
+                dispatch({ type: 'users/get', payload: users })
+            }
+
+            fetchUsers()
+
             // save the {email, token} in the localStorage
             localStorage.setItem('user', JSON.stringify(json))
 
